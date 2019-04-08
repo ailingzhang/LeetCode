8d135e2f2f18d96c6a14bcdce6edf8031f061093
ailingzhang <1772911028@qq.com>
2019-04-08 15:29:40 +0800

    链表

---
+++ 206.反转链表.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (59.72%)
+ * Total Accepted:    47.4K
+ * Total Submissions: 79.3K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 反转一个单链表。
+ * 
+ * 示例:
+ * 
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode*next;
+        ListNode*pre=NULL;
+        
+        while(head!=NULL){
+            next=head->next;//注意拼写ListNode与listcode；最先赋值
+            head->next=pre;
+            pre=head;
+            head=next;          
+        }
+
+        return pre;
+    }
+};
+
